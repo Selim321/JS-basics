166a28640c5c53031ba4ac518b355192576bab7c
Selim Sahrane <s.sahrane321@gmail.com>
2024-06-06 18:53:24 +0100

    Initial commit

---
+++ variables.js
@@ -0,0 +1,15 @@
+let age = 25; // Number
+const name = 'John'; // String
+var isStudent = true; // Boolean
+
+let user = { // Object
+  name: 'Alice',
+  age: 30
+};
+
+let fruits = ['apple', 'banana', 'cherry']; // Array (special type of object)
+
+let book_title = "Les misérables";
+let book_author = "Victor Hugo";
+
+console.log(book_title);
